371d9422300d0b6fc0095bcf1b77953fbfe6521d
Ivan Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2024-03-08 14:35:47 +0000

    agregando yatzy unitTest

--- python/test_yatzy.py
+++ python/test_yatzy.py
@@ -7,4 +7,18 @@ from yatzy import Yatzy
 def test_demo():
         expected = 15
         actual = 15
-        assert expected == actual        
+        assert expected == actual  
+
+def test_YatzyRuleAllDiceHaveTheSameNumberThenGetScore50():
+        dice = [1,1,1,1,1]
+        expected = 50
+        actual = Yatzy.yatzy(dice)
+
+        assert expected == actual
+
+def test_YatzyRuleExistsAnDiferentDiceThenGetScore0():
+        dice = [1,1,1,2,1]
+        expected = 0
+        actual = Yatzy.yatzy(dice)
+
+        assert expected == actual
